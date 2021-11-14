6bdc0347acec9dc7cf15b873a5293715db99542f
YangChaoqun <51304156+HappyThis@users.noreply.github.com>
2021-11-14 15:41:25 +0800

    文件选择功能

--- app/src/main/java/com/example/workapplication/MainActivity.java
+++ app/src/main/java/com/example/workapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -18,6 +19,7 @@ import android.widget.Toolbar;
 
 import com.example.workapplication.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -48,16 +50,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-//        MenuItem item=findViewById(R.menu.toolbar);
-//        item.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                Log.e("hahah","hahah");
-//                //startActivityForResult(i, RESULT_LOAD_IMAGE);
-//
-//            }
-//        });
 
     }
 
@@ -65,19 +57,64 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
-}
-//public boolean onOptionsItemSelected(MenuItem item){
-//        switch(item.getItemId()){
-//            case R.id.add:
-//                setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                        startActivityForResult(i, RESULT_LOAD_IMAGE);
-//                        popupWindow.dismiss();
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.send) {
+            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, 6);
+            return true;
+        }
+        if (id == R.id.user) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 6 && null != data) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                final String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+                Log.e("pic", picturePath);
+            }
+        }
+    }
+
+
+//    //将bitmap转化为png格式
+//    public File saveMyBitmap(Bitmap mBitmap) {
+//        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+//        File file = null;
+//        try {
+//            file = File.createTempFile(
+//                    UploadAccess.generateFileName(),  /* prefix */
+//                    ".jpg",         /* suffix */
+//                    storageDir      /* directory */
+//            );
 //
-//                    }
-//                });
+//            FileOutputStream out = new FileOutputStream(file);
+//            mBitmap.compress(Bitmap.CompressFormat.JPEG, 20, out);
+//            out.flush();
+//            out.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
 //        }
-//}
+//        return file;
+//    }
+}
 
