56c79939061604d8183580c1cefc660c36126f44
YangChaoqun <51304156+HappyThis@users.noreply.github.com>
2021-11-16 19:31:27 +0800

    异步任务+图片文字展示+asset

--- app/src/main/java/com/example/workapplication/ui/WelcomeActivity.java
+++ app/src/main/java/com/example/workapplication/ui/WelcomeActivity.java
@@ -1,18 +1,33 @@
 package com.example.workapplication.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
 import com.example.workapplication.MainActivity;
 import com.example.workapplication.R;
 import com.example.workapplication.Login;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -24,52 +39,88 @@ public class WelcomeActivity extends Activity {
     private Handler handler;
     private Runnable runnable;
     private boolean isClick;
+    private String path;
+    private String words;
+    Timer welcome = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        isClick=false;
+        isClick = false;
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.new_welcome);
+        //加载json
+        String fileName = "welcome.json";
+        String myjson = getJson(this, fileName);
+        JSONObject jb = JSONObject.parseObject(myjson);
+        Object path = jb.get("picPath");
+        Object words = jb.get("words");
+        this.path = path.toString();
+        this.words = words.toString();
+        Log.e("picPath", path.toString());
+        Log.e("words", words.toString());
         int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
         getWindow().setFlags(flag, flag);
         initView();
         timer.schedule(task, 1000, 1000);//等待一秒，停顿一秒
+        welcome.schedule(welcomeTask, 0, 1000);
         handler = new Handler();
         handler.postDelayed(runnable = new Runnable() {
             @Override
             public void run() {
-                if (!isClick){
+                if (!isClick) {
                     Intent intent = new Intent(WelcomeActivity.this, Login.class);
                     startActivity(intent);
                     finish();
                 }
             }
-        }, count* 1000L);
+        }, count * 1000L);
     }
-    private void initView(){
-        textView=findViewById(R.id.textView);
+
+    private void initView() {
+        textView = findViewById(R.id.textView);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isClick=true;
+                isClick = true;
                 Intent intent = new Intent(WelcomeActivity.this, Login.class);  //页面跳转到登录界面
                 startActivity(intent);
                 finish();
-                }
+            }
         });
     }
-    TimerTask task=new TimerTask() {
+
+    public static String getJson(Context context, String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        //获得assets资源管理器
+        AssetManager assetManager = context.getAssets();
+        //使用IO流读取json文件内容
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    assetManager.open(fileName), "utf-8"));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+
+    TimerTask task = new TimerTask() {
         @Override
         public void run() {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    textView=findViewById(R.id.textView);
-                    if (count>0){
-                        textView.setText("跳过"+count+"");
-                        count-=1;
-                    }else{
+                    textView = findViewById(R.id.textView);
+                    if (count > 0) {
+                        textView.setText("跳过" + count + "");
+                        count -= 1;
+                    } else {
                         timer.cancel();
                     }
 
@@ -77,6 +128,44 @@ public class WelcomeActivity extends Activity {
             });
         }
     };
+    TimerTask welcomeTask = new TimerTask() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ImageView img = findViewById(R.id.imageView);
+                    Bitmap file = getImageFromAssetsFile(path);
+                    img.setImageBitmap(file);
+                    textView = findViewById(R.id.textView);
+                    textView.setText(words);
+                    welcome.cancel();
+                }
+            });
+        }
+    };
 
+    public Bitmap getImageFromAssetsFile(String fileName) {
+        Bitmap image = null;
+        AssetManager am = getAssets();
+        InputStream is = null;
+        try {
+            is = am.open(fileName);
+            image = BitmapFactory.decodeStream(is);
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return image;
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                    is = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
 
